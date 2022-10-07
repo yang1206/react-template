59fb5cf44a4b52d938aff1ec46dd690180b44a8f
yang1206 <yang15188280673@outlook.com>
2022-10-07 13:00:20 +0800

    fix: 🧩 fix axios type

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,11 +2,8 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 import Request from './request'
 import type { RequestConfig } from './request/types'
 export interface IResponse<T> {
-  [x: string]: any
-  token: any
-  statusCode: number
-  desc: string
-  result: T
+  code: number
+  data: T
 }
 // 重写返回类型
 interface HttpRequestConfig<T, R> extends RequestConfig<IResponse<R>> {
@@ -36,7 +33,7 @@ const request = new Request({
  * @param {HttpRequestConfig} config 不管是GET还是POST请求都使用data
  * @returns {Promise}
  */
-const HttpRequest = <D = any, T = any>(config: HttpRequestConfig<D, T>): Promise<any> => {
+const HttpRequest = <D = any, T = any>(config: HttpRequestConfig<D, T>) => {
   const { method = 'GET' } = config
   if (method === 'get' || method === 'GET')
     config.params = config.data
