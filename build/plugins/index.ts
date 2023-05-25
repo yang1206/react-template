f5ba4d7531071494790a8da088aa523196b89e6e
yang1206 <yang15188280673@outlook.com>
2023-05-25 14:26:22 +0800

    fix: type

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import unocss from 'unocss/vite'
 import unplugins from './unplugin'
 
-export function setupVitePlugins(viteEnv?: ViteEnv, isBuild?: boolean): PluginOption[] {
+export function setupVitePlugins(viteEnv?: ImportMetaEnv, isBuild?: boolean): PluginOption[] {
   const plugins = [
     ...unplugins,
     react(),
