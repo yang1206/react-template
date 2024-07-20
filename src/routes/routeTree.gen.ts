65492227ea1819b486aab7f3f5d61708e13e4b9c
yang1206 <yang1206@outlook.com.tr>
2024-07-20 20:35:42 +0800

    chore: update deps

--- src/routes/routeTree.gen.ts
+++ src/routes/routeTree.gen.ts
@@ -31,10 +31,16 @@ const IndexRoute = IndexImport.update({
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
     '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
     '/count': {
+      id: '/count'
+      path: '/count'
+      fullPath: '/count'
       preLoaderRoute: typeof CountImport
       parentRoute: typeof rootRoute
     }
@@ -43,6 +49,26 @@ declare module '@tanstack/react-router' {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([IndexRoute, CountRoute])
+export const routeTree = rootRoute.addChildren({ IndexRoute, CountRoute })
 
 /* prettier-ignore-end */
+
+/* ROUTE_MANIFEST_START
+{
+  "routes": {
+    "__root__": {
+      "filePath": "__root.tsx",
+      "children": [
+        "/",
+        "/count"
+      ]
+    },
+    "/": {
+      "filePath": "index.tsx"
+    },
+    "/count": {
+      "filePath": "count.tsx"
+    }
+  }
+}
+ROUTE_MANIFEST_END */
